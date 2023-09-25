35bdfbeaac0e5f5bcc24482735b09bb0f29a380b
philipmontford <132577542+philipmontford@users.noreply.github.com>
2023-09-25 20:23:21 +0000

    Check Box
    Event lister for checkbox

--- dist/index.js
+++ dist/index.js
@@ -15,3 +15,26 @@ const checkboxMetric = document.getElementById("checkMetric");
 const checkboxImperial = document.getElementById("checkImperial");
 const formMetric = document.getElementById("metric");
 const formImperial = document.getElementById("imperial");
+let IMC = 0;
+//checkbox metric select
+checkboxMetric.onclick = function () {
+    if (checkboxMetric.checked) {
+        checkboxImperial.checked = false;
+        formMetric.classList.remove("hidden");
+        formImperial.classList.add("hidden");
+    }
+    else {
+        checkboxMetric.checked = true;
+    }
+};
+//checkbox imperial select
+checkboxImperial.onclick = function () {
+    if (checkboxImperial.checked) {
+        checkboxMetric.checked = false;
+        formImperial.classList.remove("hidden");
+        formMetric.classList.add("hidden");
+    }
+    else {
+        checkboxImperial.checked = true;
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,27 @@ const checkboxMetric = document.getElementById("checkMetric") as HTMLInputElemen
 const checkboxImperial = document.getElementById("checkImperial") as HTMLInputElement;
 const formMetric = document.getElementById("metric") as HTMLFormElement;
 const formImperial = document.getElementById("imperial") as HTMLFormElement;
+
+let IMC: number = 0;
+
+//checkbox metric select
+checkboxMetric.onclick = function () {
+  if (checkboxMetric.checked) {
+    checkboxImperial.checked = false;
+    formMetric.classList.remove("hidden");
+    formImperial.classList.add("hidden");
+  } else {
+    checkboxMetric.checked = true;
+  }
+};
+
+//checkbox imperial select
+checkboxImperial.onclick = function () {
+  if (checkboxImperial.checked) {
+    checkboxMetric.checked = false;
+    formImperial.classList.remove("hidden");
+    formMetric.classList.add("hidden");
+  } else {
+    checkboxImperial.checked = true;
+  }
+};
